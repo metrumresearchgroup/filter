d3b6acaeb450b60b8c5cd4d64f6f8837f0a446f6
Eli Sarver <elis@metrumrg.com>
2021-09-10 12:00:10 -0400

    Doc string missing on DropEmpty

--- filter.go
+++ filter.go
@@ -171,6 +171,8 @@ func (fs Funcs) applyRow(s []byte) []byte {
 	return work
 }
 
+// DropEmpty returns nil on an empty slice. This allows us
+// to eliminate rows that have no meaning in output elsewhere.
 func DropEmpty(s []byte) []byte {
 	if len(s) == 0 {
 		return nil
